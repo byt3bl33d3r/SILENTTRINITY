287d3c83d79003cf02877d473243ca0facf88255
David Tavarez <davidftavarez@gmail.com>
2019-03-16 17:59:36 -0400

    Initial commit: small changes

---
+++ SILENTTRINITY/Utilities/Crypto.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+using System.IO;
+
+namespace SILENTTRINITY.Utilities
+{
+    public static class Crypto
+    {
+        // TODO: Migrate to self implemented Diffie-Hellman Key Exchange
+        async public static Task<byte[]> KeyExchangeAsync(Uri URL, string Endpoint = "")
+        {
+            byte[] key = default(byte[]);
+
+            using (ECDiffieHellmanCng AsymAlgo = new ECDiffieHellmanCng())
+            {
+                var publicKey = AsymAlgo.PublicKey.ToXmlString();
+                byte[] response = await Http.PostAsync(URL, Encoding.UTF8.GetBytes(publicKey));
+
+                ECDiffieHellmanCngPublicKey peerPublicKey = 
+                    ECDiffieHellmanCngPublicKey.FromXmlString(Encoding.UTF8.GetString(response));
+                key = AsymAlgo.DeriveKeyMaterial(peerPublicKey);
+            }
+
+            return key;
+        }
+
+        public static byte[] Decrypt(byte[] key, byte[] data)
+        {
+            byte[] decryptedData = default(byte[]);
+
+            byte[] iv = new byte[16];
+            byte[] ciphertext = new byte[(data.Length - 32) - 16];
+            byte[] hmac = new byte[32];
+
+            Array.Copy(data, iv, 16);
+            Array.Copy(data, data.Length - 32, hmac, 0, 32);
+            Array.Copy(data, 16, ciphertext, 0, (data.Length - 32) - 16);
+
+            using (HMACSHA256 hmacsha256 = new HMACSHA256(key))
+            {
+                byte[] computedHash = hmacsha256.ComputeHash(iv.Concat(ciphertext).ToArray());
+                for (int i = 0; i < hmac.Length; i++)
+                {
+                    if (computedHash[i] != hmac[i])
+                    {
+                        return decryptedData;
+                    }
+                }
+                decryptedData = AesDecrypt(ciphertext, key, iv);
+            }
+            return decryptedData;
+        }
+
+        private static byte[] AesDecrypt(byte[] data, byte[] key, byte[] iv)
+        {
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Padding = PaddingMode.PKCS7;
+                aesAlg.KeySize = 256;
+                aesAlg.Key = key;
+                aesAlg.IV = iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                using (MemoryStream decryptedData = new MemoryStream())
+                {
+                    using (CryptoStream cryptoStream = new CryptoStream(decryptedData, decryptor, CryptoStreamMode.Write))
+                    {
+                        cryptoStream.Write(data, 0, data.Length);
+                        cryptoStream.FlushFinalBlock();
+                        return decryptedData.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ SILENTTRINITY/Utilities/Http.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace SILENTTRINITY.Utilities
+{
+    public static class Http
+    {
+        async public static Task<byte[]> GetAsync(Uri url)
+        {
+            return await new HttpClient().GetByteArrayAsync(url);
+        }
+
+        async public static Task<byte[]> PostAsync(Uri url, byte[] payload)
+        {
+            HttpClient client = new HttpClient();
+
+            ByteArrayContent content = new ByteArrayContent(payload);
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            content.Headers.ContentLength = payload.Length;
+
+            HttpResponseMessage response = await client.PostAsync(url, content);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                return default(byte[]);
+            }
+
+            return await response.Content.ReadAsByteArrayAsync();
+        }
+    }
+}
