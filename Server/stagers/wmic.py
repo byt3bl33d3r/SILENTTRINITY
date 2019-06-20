d8b3c39091ed0d5ddcf5b0583734f0f8c67e0cfb
MrFluffyBunny <jadamson08@gmail.com>
2019-06-20 20:22:54 +1000

    changed build dir

--- Server/stagers/wmic.py
+++ Server/stagers/wmic.py
@@ -20,7 +20,7 @@ class STStager:
         }
 
     def generate(self, listener, filename=None, as_string=False):
-        stager_filename = filename if filename is not None else 'wmic.xsl'
+        stager_filename = filename if filename is not None else 'artifacts/wmic.xsl'
         remote_host = self.options['rhost']['Value'] or listener['BindIP']
         remote_port = self.options['rport']['Value'] or listener['Port']
         c2_url = f"{listener.name}://{remote_host}:{remote_port}"
