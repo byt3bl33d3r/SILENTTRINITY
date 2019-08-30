51ba02b30d10f095d4ab8c3c8a5637f22d16cb1b
Daudau <daubresse.nicolas@gmail.com>
2019-08-30 23:22:13 +0200

    New module: Get-NetLocalGroupMembers equivalent

---
+++ core/teamserver/modules/boo/netlocalgroupmembers.py
@@ -0,0 +1,29 @@
+from core.teamserver.module import Module
+
+
+class STModule(Module):
+    def __init__(self):
+        self.name = 'boo/netlocalgroupmembers'
+        self.language = 'boo'
+        self.description = 'Get a list of local groups from a remote computer.'
+        self.author = '@Daudau'
+        self.references = []
+        self.options = {
+            'ComputerName': {
+                'Description': 'Computer name to query for local group members. If not set, will run on local machine.',
+                'Required': False,
+                'Value': ""
+            },
+            'GroupName': {
+                'Description': 'Local group to search for members.',
+                'Required': True,
+                'Value': "Administrators"
+            }
+        }
+
+    def payload(self):
+        with open('core/teamserver/modules/boo/src/netlocalgroupmembers.boo', 'r') as module_src:
+            src = module_src.read()
+            src = src.replace('COMPUTER_NAME', str(self.options['ComputerName']['Value']))
+            src = src.replace('GROUP_NAME', str(self.options['GroupName']['Value']))
+            return src
