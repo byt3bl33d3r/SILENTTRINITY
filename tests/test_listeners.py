c7af4234c76580593ea77806ec625c3cf9a0fb72
byt3bl33d3r <byt3bl33d3r@protonmail.com>
2019-09-26 16:47:41 -0600

    whatishappening.jpg

--- tests/test_listeners.py
+++ tests/test_listeners.py
@@ -3,6 +3,8 @@ import requests
 import sys
 import os
 import random
+from urllib3.util.retry import Retry
+from requests.adapters import HTTPAdapter
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(root_dir)
@@ -16,6 +18,15 @@ def listener_loader():
     return Loader(type="listener", paths=["core/teamserver/listeners/"])
 
 def test_listeners(listener_loader):
+    s = requests.Session()
+    retries = Retry(
+        total=3,
+        backoff_factor=0.5
+    )
+    s.verify = False
+    s.mount('http://', HTTPAdapter(max_retries=retries))
+    s.mount('https://', HTTPAdapter(max_retries=retries))
+
     for l in listener_loader.loaded:
         print(f"Testing listener '{l.name}'")
         assert isinstance(l, Listener) == True
@@ -24,7 +35,7 @@ def test_listeners(listener_loader):
             l['BindIP'] = '127.0.0.1'
             l['Port'] = str(random.randint(3000, 6000))
             l.start()
-            r = requests.get(f"{l.name}://127.0.0.1:{l['Port']}/", verify=False)
+            r = s.get(f"{l.name}://127.0.0.1:{l['Port']}/")
             assert r.status_code == 404
             l.stop()
             assert l.running == False
