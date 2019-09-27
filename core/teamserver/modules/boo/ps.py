83abd6bcb86860980dd8a7ba9b74618739dd7b27
thewover <thewover@protonmail.ch>
2019-09-26 23:42:41 -0500

    Added color legend to description, green for managed processes, attributes (blink, bold, etc.) for different categories

--- core/teamserver/modules/boo/ps.py
+++ core/teamserver/modules/boo/ps.py
@@ -7,7 +7,20 @@ class STModule(Module):
     def __init__(self):
         self.name = 'boo/ps'
         self.language = 'boo'
-        self.description = 'Discover processes, what properties those processes have and whether or not they have the CLR loaded'
+        self.description = """Discover processes, what properties those processes have and whether or not they have the Common Language Runtime (CLR) loaded.
+        
+        Colors:
+        Yellow = Current process
+        Magenta = Web browser
+        Blue = Background Windows process (explorer/winlogon/lsass)
+        Red = AV/HIDS
+        Cyan = SysAdmin / security tools
+        White = Developer tools, guest virtualization tools, misc.
+        Green = Managed process (has the .NET CLR loaded). Safer for injecting into than unmanaged processes.
+        
+        Attributes:
+        Blinking = Caution! Affects your OpSec.
+        Bold = Injectable high-integrity process"""
         self.author = "@TheWover (original C# version), @byt3bl33d3r (Boolang port), list of processes taken from @r3dQu1nn's ProcessColor.cna script (https://github.com/harleyQu1nn/AggressorScripts)"
         self.references = []
         self.options = {
@@ -39,7 +52,7 @@ class STModule(Module):
 
         admin = ["MobaXterm.exe", "bash.exe", "git-bash.exe", "mmc.exe", "Code.exe", "notepad++.exe", "notepad.exe", "cmd.exe", "drwatson.exe", "DRWTSN32.EXE", "drwtsn32.exe", "dumpcap.exe", "ethereal.exe", "filemon.exe", "idag.exe", "idaw.exe", "k1205.exe", "loader32.exe", "netmon.exe", "netstat.exe", "netxray.exe", "NmWebService.exe", "nukenabber.exe", "portmon.exe", "powershell.exe", "PRTG Traffic Gr.exe", "PRTG Traffic Grapher.exe", "prtgwatchdog.exe", "putty.exe", "regmon.exe", "SystemEye.exe", "taskman.exe", "TASKMGR.EXE", "tcpview.exe", "Totalcmd.exe", "TrafMonitor.exe", "windbg.exe", "winobj.exe", "wireshark.exe", "WMonAvNScan.exe", "WMonAvScan.exe", "WMonSrv.exe","regedit.exe", "regedit32.exe", "accesschk.exe", "accesschk64.exe", "AccessEnum.exe", "ADExplorer.exe", "ADInsight.exe", "adrestore.exe", "Autologon.exe", "Autoruns.exe", "Autoruns64.exe", "autorunsc.exe", "autorunsc64.exe", "Bginfo.exe", "Bginfo64.exe", "Cacheset.exe", "Clockres.exe", "Clockres64.exe", "Contig.exe", "Contig64.exe", "Coreinfo.exe", "ctrl2cap.exe", "Dbgview.exe", "Desktops.exe", "disk2vhd.exe", "diskext.exe", "diskext64.exe", "Diskmon.exe", "DiskView.exe", "du.exe", "du64.exe", "efsdump.exe", "FindLinks.exe", "FindLinks64.exe", "handle.exe", "handle64.exe", "hex2dec.exe", "hex2dec64.exe", "junction.exe", "junction64.exe", "ldmdump.exe", "Listdlls.exe", "Listdlls64.exe", "livekd.exe", "livekd64.exe", "LoadOrd.exe", "LoadOrd64.exe", "LoadOrdC.exe", "LoadOrdC64.exe", "logonsessions.exe", "logonsessions64.exe", "movefile.exe", "movefile64.exe", "notmyfault.exe", "notmyfault64.exe", "notmyfaultc.exe", "notmyfaultc64.exe", "ntfsinfo.exe", "ntfsinfo64.exe", "pagedfrg.exe", "pendmoves.exe", "pendmoves64.exe", "pipelist.exe", "pipelist64.exe", "portmon.exe", "procdump.exe", "procdump64.exe", "procexp.exe", "procexp64.exe", "Procmon.exe", "PsExec.exe", "PsExec64.exe", "psfile.exe", "psfile64.exe", "PsGetsid.exe", "PsGetsid64.exe", "PsInfo.exe", "PsInfo64.exe", "pskill.exe", "pskill64.exe", "pslist.exe", "pslist64.exe", "PsLoggedon.exe", "PsLoggedon64.exe", "psloglist.exe", "pspasswd.exe", "pspasswd64.exe", "psping.exe", "psping64.exe", "PsService.exe", "PsService64.exe", "psshutdown.exe", "pssuspend.exe", "pssuspend64.exe", "RAMMap.exe", "RegDelNull.exe", "RegDelNull64.exe", "regjump.exe", "ru.exe", "ru64.exe", "sdelete.exe", "sdelete64.exe", "ShareEnum.exe", "ShellRunas.exe", "sigcheck.exe", "sigcheck64.exe", "streams.exe", "streams64.exe", "strings.exe", "strings64.exe", "sync.exe", "sync64.exe", "Sysmon.exe", "Sysmon64.exe", "Tcpvcon.exe", "Tcpview.exe", "Testlimit.exe", "Testlimit64.exe", "vmmap.exe", "Volumeid.exe", "Volumeid64.exe", "whois.exe", "whois64.exe", "Winobj.exe", "ZoomIt.exe", "KeePass.exe", "1Password.exe", "lastpass.exe", "WindowsTerminal.exe"]
 
-        dev = ["sublime_text.exe", "ipython.exe", "python.exe", "devenv.exe", "ruby.exe", "perl.exe"]
+        dev = ["sublime_text.exe", "Code.exe", "ipython.exe", "python.exe", "devenv.exe", "ruby.exe", "perl.exe"]
 
         other = ["vmtoolsd.exe", "VBoxTray.exe", "VBoxControl.exe"]
 
@@ -47,21 +60,30 @@ class STModule(Module):
         colored_output.write("------------                   ---        ----       ----       -------    -------    ---------  ----\n")
         next(output)
         for line in output:
+            attrs = []
+
+            if line.split()[6] == "High":
+                attrs.append("bold")
+
             if line.find(str(context.session.info['ProcessId'])) > 0:
                 colored_output.write(colored(line, "yellow"))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), ["chrome.exe", "firefox.exe", "iexplore.exe", "MicrosoftEdge.exe", "MicrosoftEdgeCP.exe"]))):
-                colored_output.write(colored(line, "magenta"))
+                colored_output.write(colored(line, "magenta", attrs=attrs))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), ["explorer.exe", "winlogon.exe", "lsass.exe"]))):
-                colored_output.write(colored(line, "blue"))
+                colored_output.write(colored(line, "blue", attrs=attrs))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), av))) or len(list(filter(lambda p: line.startswith(p), av1))):
-                colored_output.write(colored(line, "red"))
+                attrs.append("blink")
+                colored_output.write(colored(line, "red", attrs=attrs))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), admin))):
-                colored_output.write(colored(line, "cyan"))
+                colored_output.write(colored(line, "cyan", attrs=attrs))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), dev))):
-                colored_output.write(colored(line, "white"))
+                colored_output.write(colored(line, "white", attrs=attrs))
             elif len(list(filter(lambda p: line.startswith(p[:-4]), other))):
-                colored_output.write(colored(line, "white"))
+                attrs.append("blink")
+                colored_output.write(colored(line, "white", attrs=attrs))
+            elif line.split()[4] == "True":
+                colored_output.write(colored(line, "green", attrs=attrs))
             else:
-                colored_output.write(line)
+                colored_output.write(colored(line, attrs=attrs))
 
         return colored_output.getvalue()
